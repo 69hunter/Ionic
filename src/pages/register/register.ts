f9ab4cf8baa73e521dc232724196997786ceafa1
khunlingtan <kltan69@gmail.com>
2017-10-28 10:34:46 +0800

    photo library and call

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -64,6 +64,29 @@ export class RegisterPage {
     });
   }
 
+  getFromLibrary(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 100,
+      targetWidth: 100,
+      allowEdit: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+
+      this.image = imageData;
+      console.log(imageData);
+    }, (err) => {
+        console.log('Error obtaining picture')
+    });
+
+  }
+
   onSubmit() {
     console.log(this.registerForm.value);
     this.dismiss();
